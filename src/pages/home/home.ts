12c4bfce72be5f87fe9fb4122901258c570a2d3d
Charles Liu <dodoinblue@gmail.com>
2018-03-09 08:58:07 -0800

    BluetoothLe ionic native rapper for central mode

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BluetoothLE } from '../../wrapper/BluetoothLeWrapper'
+// import { InitParams } from '../../wrapper/models/BlePluginParams';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,43 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public ble: BluetoothLE) {
+    console.log('constructor')
+  }
 
+  init() {
+    console.log('init')
+    this.ble.initialize().subscribe(result => {
+      console.log(result)
+    })
+  }
+
+  checkPermissions() {
+    console.log('checkPermissions')
+  }
+
+  checkLocations() {
+    console.log('checkLocations')
+  }
+
+  enable() {
+    console.log('enable')
+  }
+
+  disable() {
+    console.log('disable')
+  }
+
+  getAdapterInfo() {
+    console.log('getAdapterInfo')
+  }
+
+  scanWithTimeout() {
+    console.log('scanWithTimeout')
+  }
+
+  retrieveConnected() {
+    console.log('retrieveConnected')
   }
 
 }
